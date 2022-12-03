23c7a5b92ee23800d30359a961ba2118c0c3ec6f
Melaku <melakuwondwossen111@gmail.com>
2022-12-03 07:29:21 -0900

    added readme and fixed bugs

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
-''' script for task 2 '''
+"""
+This script takes in an argument and 
+displays all values in the states
+where 'name' matches the argument
+from the database 'hbtn_0e_0_usa'
+"""
 
-
-import sys
 import MySQLdb
+from sys import argv
 
+if __name__ == '__main__':
+    """
+    Access to the database and get the states
+    from the database.
+    """
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    db = MySQLdb.connect(host="localhost",user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE BINARY name = '{:s}'"
                 .format(sys.argv[4])+"ORDER BY id ASC")
